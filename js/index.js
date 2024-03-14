c688953aa8696b4d912964764ea302805e998049
D-Sriver <duez.s@proton.me>
2024-03-14 18:26:54 +0100

    💄 Finition des card

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-import { displayRecipes } from "./Templates/recipe-card.js";
+// import { displayRecipes } from "./Templates/recipe-card.js";
 
-// Appel des fonctions
-displayRecipes();
+// // Appel des fonctions
+// displayRecipes();
 
 // Debug
 import { logInputValue } from "./Data/input-data.js";
