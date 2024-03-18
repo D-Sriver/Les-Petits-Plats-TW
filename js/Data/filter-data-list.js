441bd34966231c6c26dda778402bc68a010f43f0
D-Sriver <duez.s@proton.me>
2024-03-18 11:28:31 +0100

    ✨ Ajout de tris de la liste d'ingredient.

---
+++ js/Data/filter-data-list.js
@@ -0,0 +1,46 @@
+import { recipes } from "./recipes.js";
+
+const getIngredients = () => {
+  // crée un tableau vide pour stocker les ingrédients
+  const ingredients = recipes.reduce((uniqueIngredients, recette) => {
+    // on parcourt les ingrédients de chaque recette
+    recette.ingredients.forEach((ingredient) => {
+      // si l'ingrédient n'est pas déjà dans le tableau,
+      if (!uniqueIngredients.includes(ingredient.ingredient)) {
+        // on l'ajoute
+        uniqueIngredients.push(ingredient.ingredient);
+      }
+    });
+    // on retourne le tableau mis à jour
+    return uniqueIngredients;
+  }, []);
+  // on retourne le tableau
+  return ingredients;
+};
+
+const getAppareil = () => {
+  const appareil = recipes.reduce((uniqueAppareil, recette) => {
+    if (!uniqueAppareil.includes(recette.appliance)) {
+      uniqueAppareil.push(recette.appliance);
+    }
+    return uniqueAppareil;
+  }, []);
+  return appareil;
+};
+
+const getUstensils = () => {
+  const ustensils = recipes.reduce((uniqueUstensils, recette) => {
+    recette.ustensils.forEach((ustensil) => {
+      if (!uniqueUstensils.includes(ustensil)) {
+        uniqueUstensils.push(ustensil);
+      }
+    });
+    return uniqueUstensils;
+  }, []);
+  return ustensils;
+};
+console.dir(getIngredients());
+console.dir(getAppareil());
+console.dir(getUstensils());
+
+export { getAppareil, getIngredients, getUstensils };
--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ displayRecipes();
 // Debug
 import { logInputValue } from "./Data/input-data.js";
 logInputValue();
+
+// Import les liste filtrées pour les select
+import {
+  getAppareil,
+  getIngredients,
+  getUstensils,
+} from "./Data/filter-data-list.js";
+getIngredients();
+getAppareil();
+getUstensils();
