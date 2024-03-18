c57a6e14c00c1a048801065e0b528d8784bfdec8
D-Sriver <duez.s@proton.me>
2024-03-18 10:59:03 +0100

    🎨  Création des template javascript des recipe-card

--- js/Templates/recipe-card.js
+++ js/Templates/recipe-card.js
@@ -4,40 +4,40 @@ const recipeContainer = document.querySelector(".recipes");
 const generateRecipeHTML = (recipe) => {
   // crée les carte pour chaque recette en utilisant les données de recipes.js
   return `
-    <article class="fle flex-col rounded-lg mx-auto max-w-md">
-      <img class="object-cover rounded-t-lg" src="img/Recipe/${
-        recipe.image
-      }" alt="repas" />
-      <div class=" p-6 recipes-description">
-        <div class="flex justify-between items-center">
-          <h3 class="font-bold text-lg">${recipe.name}</h3>
-          <div class= "bg-yellow-300 text-black rounded-full px-2 py-1 text-xs font-bold">${
-            recipe.time
-          } min</div>
+  <div class="bg-white rounded-3xl ">
+      <div class="relative rounded-t-3xl pb-4">
+        <span class="absolute top-6 right-4 bg-yellow-400 text-xs px-4 py-2 rounded-3xl">${
+          recipe.time
+        } min</span>
+        <img src="/img/Recipe/${
+          recipe.image
+        }" alt="repas" class="w-full h-56 object-cover rounded-t-3xl">
+      </div>
+      <div class="px-8 pt-4">
+        <div class="pb-5">
+          <h2 class="font-anton text-lg">${recipe.name}</h2>
         </div>
-        <span class="uppercase">Recette</span>
-        <p>${recipe.description}</p>
-        <span class="uppercase">Ingrédients</span>
-        <div class="ingredients-container">
-          ${recipe.ingredients
-            .map(
-              (ingredient) => `
-            <div>
-              <span>${ingredient.ingredient}</span>
-              ${
-                ingredient.quantity
-                  ? `<span class="quantity">${ingredient.quantity}${
-                      ingredient.unit || ""
-                    }</span>`
-                  : ""
-              }
-            </div>
-          `
-            )
-            .join("")}
+        <h3 class="font-semibold tracking-wider text-gray-400 uppercase mb-4">Recette</h3>
+        <p class="font-light pb-10">${recipe.description}</p>
+        <div class="mb-4">
+          <h3 class="text-xs font-semibold text-gray-500 uppercase mb-4">Ingrédients</h3>
+          <ul class="grid grid-cols-2 gap-3 pb-8">
+            ${recipe.ingredients
+              .map(
+                (ingredient) => `
+                <li class="grid" >
+                  <span>${ingredient.ingredient}</span>
+                  <span class="text-gray-400">${ingredient.quantity}${
+                  ingredient.unit || ""
+                }</span>
+                </li>
+              `
+              )
+              .join("")}
+          </ul>
         </div>
       </div>
-    </article>
+    </div>
   `;
 };
 // affiche les recettes
