8d3c9c1024029457a8d953e53e5f32aad8740a2b
D-Sriver <duez.s@proton.me>
2024-03-28 16:51:41 +0100

    ✏️ Fix doublon et Lettre minuscule dans le filtre

--- js/Data/filter-data-list.js
+++ js/Data/filter-data-list.js
@@ -1,46 +1,44 @@
 import { recipes } from "/js/Data/recipes.js";
 
+const capitalizeFirstLetter = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
 const getIngredients = () => {
-  // crée un tableau vide pour stocker les ingrédients
   const ingredients = recipes.reduce((uniqueIngredients, recette) => {
-    // on parcourt les ingrédients de chaque recette
     recette.ingredients.forEach((ingredient) => {
-      // si l'ingrédient n'est pas déjà dans le tableau,
-      if (!uniqueIngredients.includes(ingredient.ingredient)) {
-        // on l'ajoute
-        uniqueIngredients.push(ingredient.ingredient);
+      const lowerCaseIngredient = ingredient.ingredient.toLowerCase();
+      if (!uniqueIngredients.includes(lowerCaseIngredient)) {
+        uniqueIngredients.push(lowerCaseIngredient);
       }
     });
-    // on retourne le tableau mis à jour
     return uniqueIngredients;
   }, []);
-  // on retourne le tableau
-  return ingredients;
+  return ingredients.sort().map(capitalizeFirstLetter);
 };
 
 const getAppareil = () => {
   const appareil = recipes.reduce((uniqueAppareil, recette) => {
-    if (!uniqueAppareil.includes(recette.appliance)) {
-      uniqueAppareil.push(recette.appliance);
+    const lowerCaseAppareil = recette.appliance.toLowerCase();
+    if (!uniqueAppareil.includes(lowerCaseAppareil)) {
+      uniqueAppareil.push(lowerCaseAppareil);
     }
     return uniqueAppareil;
   }, []);
-  return appareil;
+  return appareil.sort().map(capitalizeFirstLetter);
 };
 
 const getUstensils = () => {
   const ustensils = recipes.reduce((uniqueUstensils, recette) => {
     recette.ustensils.forEach((ustensil) => {
-      if (!uniqueUstensils.includes(ustensil)) {
-        uniqueUstensils.push(ustensil);
+      const lowerCaseUstensil = ustensil.toLowerCase();
+      if (!uniqueUstensils.includes(lowerCaseUstensil)) {
+        uniqueUstensils.push(lowerCaseUstensil);
       }
     });
     return uniqueUstensils;
   }, []);
-  return ustensils;
+  return ustensils.sort().map(capitalizeFirstLetter);
 };
-// console.dir(getIngredients());
-// console.dir(getAppareil());
-// console.dir(getUstensils());
 
 export { getAppareil, getIngredients, getUstensils };
