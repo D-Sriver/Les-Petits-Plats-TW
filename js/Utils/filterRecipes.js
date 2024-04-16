d555f101ef3d1b6da5c7cde57f4df9b368f25d69
D-Sriver <duez.s@proton.me>
2024-04-16 17:08:11 +0200

    🐛  fix la recherche combinée

--- js/Utils/filterRecipes.js
+++ js/Utils/filterRecipes.js
@@ -1,21 +1,44 @@
-//TODO:  fetch data , create [] -> créer copie (résultat recherche + manip) -> recuperation initiale du [] pour filtrer le tableau.
-//TODO: boucler sur les mot multiples.
 import { recipes } from "../Data/recipes.js";
 import { UserData } from "../Data/userData.js";
 
-// Fonction pour filtrer les recettes en fonction de sortedAppareils, sortedIngredients et sortedUstensils
+let filteredRecipes = [...recipes]; // créer une copie des recettes
 
 export function filterRecipes(searchTerm) {
-  // Combinez le terme de recherche de l'utilisateur avec les données extraites des clics précédents
+  // Je crée un tableau avec les termes de recherche
   const combinedSearchTerms = [...UserData, searchTerm];
-  const filteredRecipes = recipes.filter((recipe) => {
-    return combinedSearchTerms.every(
-      (term) =>
-        recipe.name.toLowerCase().includes(term.toLowerCase()) ||
+  filteredRecipes = [...recipes]; // je réinitialise les recettes filtrées
+
+  // je filtre les recettes en fonction des termes de recherche
+  combinedSearchTerms.forEach((term) => {
+    filteredRecipes = filteredRecipes.filter((recipe) => {
+      // Vérifie si le terme de recherche est présent dans le nom de la recette
+      const recipeNameLower = recipe.name.toLowerCase();
+      if (recipeNameLower.includes(term.toLowerCase())) {
+        return true;
+      }
+
+      if (
         recipe.ingredients.some((ingredient) =>
           ingredient.ingredient.toLowerCase().includes(term.toLowerCase())
         )
-    );
+      ) {
+        return true;
+      }
+
+      if (recipe.appliance.toLowerCase().includes(term.toLowerCase())) {
+        return true;
+      }
+
+      if (
+        recipe.ustensils.some((ustensil) =>
+          ustensil.toLowerCase().includes(term.toLowerCase())
+        )
+      ) {
+        return true;
+      }
+
+      return false;
+    });
   });
 
   return filteredRecipes;
