efb29661e7a30c6ced25f216a4bc71eba8490722
D-Sriver <duez.s@proton.me>
2024-04-19 15:49:22 +0200

    🎨 passage de if a switch

--- js/Utils/filterRecipes.js
+++ js/Utils/filterRecipes.js
@@ -6,38 +6,35 @@ let filteredRecipes = [...recipes]; // créer une copie des recettes
 export function filterRecipes(searchTerm) {
   // Je crée un tableau avec les termes de recherche
   const combinedSearchTerms = [...UserData, searchTerm];
-  filteredRecipes = [...recipes]; // je réinitialise les recettes filtrées
 
   // je filtre les recettes en fonction des termes de recherche
   combinedSearchTerms.forEach((term) => {
     filteredRecipes = filteredRecipes.filter((recipe) => {
-      // Vérifie si le terme de recherche est présent dans le nom de la recette
-      const recipeNameLower = recipe.name.toLowerCase();
-      if (recipeNameLower.includes(term.toLowerCase())) {
-        return true;
-      }
+      let matchFound = false;
 
-      if (
-        recipe.ingredients.some((ingredient) =>
+      switch (true) {
+        case recipe.name.toLowerCase().includes(term.toLowerCase()):
+          matchFound = true;
+          break;
+        case recipe.ingredients.some((ingredient) =>
           ingredient.ingredient.toLowerCase().includes(term.toLowerCase())
-        )
-      ) {
-        return true;
-      }
-
-      if (recipe.appliance.toLowerCase().includes(term.toLowerCase())) {
-        return true;
-      }
-
-      if (
-        recipe.ustensils.some((ustensil) =>
+        ):
+          matchFound = true;
+          break;
+        case recipe.appliance.toLowerCase().includes(term.toLowerCase()):
+          matchFound = true;
+          break;
+        case recipe.ustensils.some((ustensil) =>
           ustensil.toLowerCase().includes(term.toLowerCase())
-        )
-      ) {
-        return true;
+        ):
+          matchFound = true;
+          break;
+        default:
+          matchFound = false;
       }
 
-      return false;
+      // Retourner le résultat du filtre
+      return matchFound;
     });
   });
 
