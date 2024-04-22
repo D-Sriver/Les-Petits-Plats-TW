6f7bf3885e345149c34ce16a9c36c8dfe7170b79
D-Sriver <duez.s@proton.me>
2024-04-22 12:05:34 +0200

    🐛 Affiche toute les recette si UserData est vide

--- js/Templates/tag.js
+++ js/Templates/tag.js
@@ -1,6 +1,9 @@
 //
+import { recipes } from "../Data/recipes.js";
 import { UserData } from "../Data/userData.js";
+import { createTagsFromArray } from "../Utils/TagBuild.js";
 import { displayRecipes } from "../Utils/displayRecipe.js";
+import { filterRecipes } from "../Utils/filterRecipes.js";
 import { updateRecipeCount } from "../Utils/search.js";
 
 function tagStructure(tagTextElement, tagContainer) {
@@ -24,10 +27,24 @@ function tagStructure(tagTextElement, tagContainer) {
   const closeButton = document.createElement("i");
   closeButton.classList.add("fas", "fa-times", "cursor-pointer", "p-2");
   closeButton.addEventListener("click", () => {
-    console.log(UserData);
-    displayRecipes(UserData);
-    updateRecipeCount(UserData);
-    //réactualise l'affichage des recettes
+    // Supprimer le tag de UserData
+    const clickedTag = tagTextElement.textContent;
+    const tagIndex = UserData.indexOf(clickedTag);
+    if (tagIndex !== -1) {
+      UserData.splice(tagIndex, 1);
+    }
+    // Rafraîchir les tags affichés
+    tagContainer.innerHTML = "";
+    createTagsFromArray(UserData, tagContainer);
+    // si UserData n'est pas vide, filtrer les recettes
+    if (UserData.length > 0) {
+      displayRecipes(filterRecipes(recipes, UserData));
+      updateRecipeCount(filterRecipes(recipes, UserData));
+    } else {
+      //afficher toutes les recettes
+      displayRecipes(recipes);
+      updateRecipeCount(recipes);
+    }
   });
 
   newTagElement.appendChild(tagTextElement);
