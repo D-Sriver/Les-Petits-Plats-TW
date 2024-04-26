90f9e50f268a3c67ccea916cfce77751e728ab85
D-Sriver <duez.s@proton.me>
2024-04-26 15:21:09 +0200

    ✨ filtre en boucle native

--- js/Utils/filterRecipes.js
+++ js/Utils/filterRecipes.js
@@ -3,48 +3,44 @@ import { UserData } from "../Data/userData.js";
 
 let filteredRecipes = [...recipes]; // créer une copie des recettes
 
-export function filterRecipes(searchTerm) {
-  // Je crée un tableau avec les termes de recherche
+function filterRecipes(searchTerm) {
   const combinedSearchTerms = [...UserData];
 
-  // Vérifier si searchTerm est défini et est une chaîne de caractères
   if (typeof searchTerm === "string" && searchTerm.trim() !== "") {
     combinedSearchTerms.push(searchTerm.trim());
   }
 
-  // Réinitialiser les recettes filtrées
   let filteredRecipes = [...recipes];
 
-  // je filtre les recettes en fonction des termes de recherche
-  combinedSearchTerms.forEach((term) => {
+  for (let i = 0; i < combinedSearchTerms.length; i++) {
+    const term = combinedSearchTerms[i].toLowerCase();
+
     filteredRecipes = filteredRecipes.filter((recipe) => {
       let matchFound = false;
 
-      switch (true) {
-        case recipe.name.toLowerCase().includes(term.toLowerCase()):
-          matchFound = true;
-          break;
-        case recipe.ingredients.some((ingredient) =>
-          ingredient.ingredient.toLowerCase().includes(term.toLowerCase())
-        ):
-          matchFound = true;
-          break;
-        case recipe.appliance.toLowerCase().includes(term.toLowerCase()):
-          matchFound = true;
-          break;
-        case recipe.ustensils.some((ustensil) =>
-          ustensil.toLowerCase().includes(term.toLowerCase())
-        ):
-          matchFound = true;
-          break;
-        default:
-          matchFound = false;
+      if (recipe.name.toLowerCase().includes(term)) {
+        matchFound = true;
+      } else if (recipe.appliance.toLowerCase().includes(term)) {
+        matchFound = true;
+      } else if (
+        recipe.ingredients.some((ingredient) =>
+          ingredient.ingredient.toLowerCase().includes(term)
+        )
+      ) {
+        matchFound = true;
+      } else if (
+        recipe.ustensils.some((ustensil) =>
+          ustensil.toLowerCase().includes(term)
+        )
+      ) {
+        matchFound = true;
       }
 
-      // Retourner le résultat du filtre
       return matchFound;
     });
-  });
+  }
 
   return filteredRecipes;
 }
+
+export { filterRecipes };
